80a52941267528b1917f2ce6c5609a0d1bc37b0b
Dimitar <dim_kolev2002@abv.bg>
2020-03-11 16:48:02 +0200

    doing project

---
+++ ReversePolishNotation/DynamicStack.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReversePolishNotation
+{
+    public class DynamicStack <T>
+    {
+        public class Node
+        {
+            public T Item { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T item)
+            {
+                this.Item = item;
+                this.Next = null;
+            }
+
+            public Node(T item, Node next)
+            {
+                this.Item = item;
+                this.Next = next;
+            }
+        }
+
+        private Node top;
+        private int count;
+        public Node Top
+        {
+            get { return this.top; }
+            set { this.top = value; }
+        }
+        public int Count
+        {
+            get => count;
+        }
+        public DynamicStack()
+        {
+            this.top = null;
+            this.count = 0;
+        }
+        public void Push(T item)
+        {
+            Node current = new Node(item, this.top);
+            this.top = current;
+            this.count++;
+        }
+        public T Pop()
+        {
+            var el = this.top.Item;
+            this.top = this.top.Next;
+            this.count--;
+            return el;
+        }
+        public T Peek()
+        {
+            var el = this.top.Item;
+            return el;
+        }
+        public void Clear()
+        {
+            this.top = null;
+            this.count = 0;
+        }
+        public T[] ToAray()
+        {
+            T[] arr = new T[this.Count];
+            Node currentNode = this.top;
+            for (int i = this.Count - 1; i >= 0; i--)
+            {
+                arr[i] = currentNode.Item;
+                currentNode = currentNode.Next;
+            }
+            return arr;
+        }
+    }
+}
---
+++ ReversePolishNotation/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReversePolishNotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string izraz = Console.ReadLine();
+            DynamicStack<char> stack = new DynamicStack<char>();
+            string obratenpolskizapis = "";
+            foreach (var znak in izraz)
+            {
+                if (znak == '(') stack.Push(znak);
+                else if (znak == ')')
+                {
+                    while (stack.Count > 0 && stack.Peek() != '(')
+                        obratenpolskizapis += (stack.Pop());
+                    stack.Pop();
+                }
+                else if ((znak >= '0' && znak <= '9' || znak == '.')||(znak>= 'a' && (znak <= 'z')))
+                {
+                   obratenpolskizapis+=znak;
+                }
+                else if (znak == '-' || znak== '+' || znak == '*' || znak == '/')
+                {
+                    while (stack.Count > 0 && stack.Peek() != '(' && Prior(znak) <= Prior(stack.Peek()))
+                    {
+                        obratenpolskizapis += (stack.Pop());
+                    }
+                    stack.Push(znak);
+                }
+                else
+                {
+                    char y = stack.Pop();
+                    if (y != '(') obratenpolskizapis += y;
+                }
+
+            }
+            while (stack.Count > 0)
+            {
+                obratenpolskizapis+=stack.Pop();
+            }
+            Console.WriteLine(obratenpolskizapis);
+
+
+            string expr = "(" + izraz + ")";
+            DynamicStack<char> znaci = new DynamicStack<char>();
+            DynamicStack<double> stojnosti = new DynamicStack<double>();
+            foreach (var znak in expr)
+            {
+                if (znak== '(') { }
+                else if (znak=='+') znaci.Push(znak);
+                else if (znak=='-') znaci.Push(znak);
+                else if (znak=='*') znaci.Push(znak);
+                else if (znak=='/') znaci.Push(znak);
+                else if (znak==')')
+                {
+                    int count = znaci.Count;
+                    while (count > 0)
+                    {
+                        char op = znaci.Pop();
+                        double v = stojnosti.Pop();
+                        if (op=='+') v = stojnosti.Pop() + v;
+                        else if (op=='-') v = stojnosti.Pop() - v;
+                        else if (op == '*') v = stojnosti.Pop() * v;
+                        else if (op == '/') v = stojnosti.Pop() / v;
+                        stojnosti.Push(v);
+
+                        count--;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"znak={znak}");
+                    stojnosti.Push(double.Parse(znak.ToString()));
+                }
+            }
+            Console.WriteLine(stojnosti.Pop());
+        }
+        static int Prior(char c)
+        {
+            switch (c)
+            {
+                case '=':
+                    return 1;
+                case '+':
+                    return 2;
+                case '-':
+                    return 2;
+                case '*':
+                    return 3;
+                case '/':
+                    return 3;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
